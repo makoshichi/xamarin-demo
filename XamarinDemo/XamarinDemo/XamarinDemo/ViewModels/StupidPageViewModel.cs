05e8c158569216ed65ca4f7a60c688d6518f759d
Eduardo Hoeller <eduardo.hoeller@outlook.com>
2019-07-25 11:55:10 -0300

    hahah

---
+++ XamarinDemo/XamarinDemo/XamarinDemo/ViewModels/StupidPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinDemo.ViewModels
+{
+    public class StupidPageViewModel
+    {
+        public string BoundName = "André zé buceta";
+    }
+}
---
+++ XamarinDemo/XamarinDemo/XamarinDemo/Views/StupidPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinDemo.ViewModels;
+
+namespace XamarinDemo
+{
+    public partial class StupidPage : ContentPage
+    {
+        public StupidPage()
+        {
+            InitializeComponent();
+            BindingContext = new StupidPageViewModel();
+        }
+    }
+}
